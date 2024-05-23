b05c1a429bf0d4b05d3dcbf5d9bd0eef3368354d
jomeli <jomeli@taltech.ee>
2024-05-23 17:37:33 +0300

    readme

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ import time
 #import smbus
 from datetime import datetime, timedelta
 import os
+import json
 
 
 class StorageBoxes:
     def __init__(self):
         self.units = []
-        self.add_unit(1,1234)
-        self.add_unit(2,0000)
+        self.pin_len = 6
+        #self.units.append(UnitInfo(21,1,1234))
+        #self.units.append(UnitInfo(21,2,4321))
+        self.units = self.load_unit_info_list()
+        #self.save_unit_info_list(self.units)
         self.run_admin_web()
         self.open_customer_gui()
+
+    def save_unit_info_list(self,unit_info_list):
+        with open("andmed.txt", 'w') as file:
+            dict_list = [unit_info.__dict__ for unit_info in unit_info_list]
+            json.dump(dict_list, file, indent=4)
+
+    def load_unit_info_list(self):
+        with open("andmed.txt", 'r') as file:
+            dict_list = json.load(file)
+            return [UnitInfo(**unit_dict) for unit_dict in dict_list]
 
 
     def add_unit(self, id, code_):
@@ -39,7 +53,7 @@ class StorageBoxes:
         data_size = 1
         data = [data_size, data_]
         slave_address = 21
-        #bus = smbus.SMBus(1)
+#        bus = smbus.SMBus(1)
         print("Sending to address:", slave_address)
         #see peaks hear beati minema
         if data_ == self.units[0].id:
@@ -49,16 +63,16 @@ class StorageBoxes:
             print("leitud id 2")
             self.units[1].door_state = 1
 
-        #bus.write_i2c_block_data(slave_address, command, data)
+#        bus.write_i2c_block_data(slave_address, command, data)
 
     def reboot_slave(self):
         command = 0x09
         data_size = 1
         data = [data_size, 0]
         slave_address = 21
-        #bus = smbus.SMBus(1)
+        bus = smbus.SMBus(1)
         print("Sending to address:", slave_address)
-        #bus.write_i2c_block_data(slave_address, command, data)
+        bus.write_i2c_block_data(slave_address, command, data)
 
     # See hetkel ei tööta, aga siin küsitakse kappide staatust. Kutsutake ka interpret_status_bytes()
     def request_response_from_slave(self):
@@ -66,12 +80,12 @@ class StorageBoxes:
         data_size = 1
         data = [data_size, 11]
         slave_address = 21
-        #bus = smbus.SMBus(1)
+        bus = smbus.SMBus(1)
         slave_address = 21
-        #received_data = bus.read_i2c_block_data(slave_address, 11)
+        received_data = bus.read_i2c_block_data(slave_address, 11)
         print("Received data:")
-        #print(received_data)
-        #self.interpret_status_bytes(received_data)
+        print(received_data)
+        self.interpret_status_bytes(received_data)
 
     # Vastusaadud info tehakse arusaadavaks infoks ja muudetakse globaalseid muutujaid, ei ole testitud
     # Näide: 1 5 2 2
@@ -100,9 +114,9 @@ class StorageBoxes:
         data_size = 2
         data = [data_size, 1, 2]  # hetkel on pandud 1 ja 2, kuidagi peaks tegema lihtsasti skaleeritavaks
         slave_address = 21
-        #bus = smbus.SMBus(1)
+        bus = smbus.SMBus(1)
         print("Sending to address:", slave_address)
-        #bus.write_i2c_block_data(slave_address, command, data)
+        bus.write_i2c_block_data(slave_address, command, data)
 
     # iga sekundi tagant küsime infot
     def heartbeat_loop(self):
@@ -135,23 +149,27 @@ class StorageBoxes:
         return used_codes
 
     def check_code(self,code_):
-        for unit in self.units:
-            if int(code_) == unit.current_code:
-                self.open_door(unit.id)
-                return unit.id  #tagastab kapi ukse id
-
-        if code_ == "1337":
+        if code_ == "001337":
             self.open_admin()
             return -2
+        for unit in self.units:
+            if int(code_) == unit.code:
+                self.open_door(unit.id)
+                return unit.id  #tagastab kapi ukse id
         return -1 #ei leitud, vigane kood
 
     def generate_code(self,door):
-        self.new_code = str(random.randint(1000, 9999))
+        self.new_code = str(random.randint(0, 999999))
+        while len(self.new_code) < self.pin_len:
+            self.new_code = "0" + self.new_code
         for unit in self.units:
-            if str(unit.current_code) == self.new_code:
-                self.new_code = str(random.randint(1000, 9999))
-            if unit.id == door:
-                unit.current_code = self.new_code
+            if str(unit.code) == self.new_code:
+                self.new_code = str(random.randint(0, 999999))
+                while len(self.new_code) < self.pin_len:
+                    self.new_code = "0" + self.new_code
+        self.units[door-1].code = self.new_code
+        return self.new_code
+
 
     def view_codes(self):
         try:
@@ -186,7 +204,7 @@ class AdminWeb:
 
     def run_web_app(self):
         # Run the Flask web app
-        self.app.run(host='192.168.11.9', port=5000)
+        self.app.run(host='0.0.0.0', port=5000)
 
     def veiw_codes_admin(self):
         @self.app.route('/view_codes')
@@ -250,26 +268,30 @@ class AdminPanel:
         self.frame = Box(self.admin_window, layout="grid")
 
         # Create the first box
-        self.box1 = Box(self.frame, border=True, grid=[0, 0],width=144,height=144)
+        self.box1 = Box(self.frame, border=True, grid=[0, 0],width=144,height=256)
         self.number1 = Text(self.box1, text="1", grid=[0, 0])
         self.box1_door = Text(self.box1, text="Closed", grid=[1, 0])
         self.box1_content = Text(self.box1, text="Empty", grid=[2, 0])
-        self.code1 = Text(self.box1, text=self.generate_code(), grid=[3, 0])
+        self.code1 = Text(self.box1, text=self.storage_boxes.units[0].code, grid=[3, 0])
         self.door_button_1 = PushButton(self.box1, text="Ava", grid=[4, 0], command=self.open_door, args=[1])
+        self.generage_button_1 = PushButton(self.box1, text="Genereeri", grid=[5, 0], command=self.generate_code, args=[1])
 
         # Create the second box
-        self.box2 = Box(self.frame, border=True, grid=[1, 0],width=144,height=144)
+        self.box2 = Box(self.frame, border=True, grid=[1, 0],width=144,height=256)
         self.number2 = Text(self.box2, text="2", grid=[1, 0])
         self.box2_door = Text(self.box2, text="Closed", grid=[2, 0])
         self.box2_content = Text(self.box2, text="Full", grid=[3, 0])
-        self.code2 = Text(self.box2, text=self.generate_code(), grid=[4, 0])
+        self.code2 = Text(self.box2, text=self.storage_boxes.units[1].code, grid=[4, 0])
         self.door_button_2 = PushButton(self.box2, text="Ava", grid=[5, 0], command=self.open_door, args=[2])
+        self.generage_button_2 = PushButton(self.box2, text="Genereeri", grid=[6, 0], command=self.generate_code, args=[2])
 
         self.quit_button = PushButton(self.admin_window, text="Close", pady=20, command=self.quit_admin)
         self.update_door_condition()
 
-    def generate_code(self):
-        return 1111
+    def generate_code(self,door):
+        new_code = self.storage_boxes.generate_code(door)
+        self.update_door_condition()
+        return new_code
 
     def open_door(self,door_id):
         self.storage_boxes.open_door(door_id)
@@ -282,11 +304,13 @@ class AdminPanel:
     def update_door_condition(self):
         for unit in self.storage_boxes.units:
             if unit.id == 1:
+                self.code1.value = str(unit.code)
                 if unit.door_state == 1:
                     self.box1_door.value = "Open"
                 else:
                     self.box1_door.value = "Closed"
             if unit.id == 2:
+                self.code2.value = str(unit.code)
                 if unit.door_state == 1:
                     self.box2_door.value = "Open"
                 else:
@@ -296,12 +320,12 @@ class AdminPanel:
 class CustomerGUI:
     def __init__(self,storage_box):
         self.storage_boxes = storage_box
-        self.current_box_number = None
-        self.pin_length = 4  # You can adjust the PIN length as needed
+        self.current_box_number = 0
+        self.pin_length = 6  # You can adjust the PIN length as needed
         self.pin = ""
 
         self.app_gui = App(title ="NUTIKAPP", width= 720, height = 480)
-
+        #self.app_gui.full_screen = True
         # Create GUI elements
         self.label = Text(self.app_gui, text="Enter your PIN:",align="top")
         self.label.text_size = 20
@@ -340,18 +364,28 @@ class CustomerGUI:
         self.update_pin_entry()
 
     def try_unlock(self):
+        if len(self.pin) < self.pin_length:
+            self.app_gui.error("VIGA", "     Liiga lühike kood!     ")
         if len(self.pin) == self.pin_length:
             self.box_number = self.storage_boxes.check_code(self.pin)
             self.clear_pin()
+        if self.box_number == -1:
+            self.app_gui.error("VIGA", "     Vale kood!     ")
+        else:
+            self.app_gui.info("INFO",f"     Kapp avanes!     ")
+
 
 class UnitInfo:
-    def __init__(self,i2c, id, code):
-        self.i2c_lane = 21 ##
+    def __init__(self,i2c, id, code,lock_state,door_state,ir_sensor):
+        self.i2c = i2c ##
         self.id = id  #kapi ukse id
-        self.current_code = code #kood millega kappi saab avada
+        self.code = code #kood millega kappi saab avada
         self.lock_state = 0 #closed 1 open
         self.door_state = 0 #closed 1 open
         self.ir_sensor = 1 #occupied 0 empty
+
+    def __repr__(self):
+        return f"UnitInfo(i2c_lane={self.i2c}, id={self.id}, current_code={self.code})"
 
     def get_code(self):
         return self.current_code
